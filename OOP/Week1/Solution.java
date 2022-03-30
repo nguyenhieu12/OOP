e8d60c0ca517095aab790d517e198a344629abec
nguyenhieu12 <vailinhhon098@gmail.com>
2022-03-30 21:38:23 +0700

    OOP

---
+++ OOP/Week1/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    // Type your main code here
+    public static void main(String[] args) {
+        System.out.println(printHello());
+    }
+
+    public static String printHello() {
+        return "HelloWorld";
+    }
+} 
